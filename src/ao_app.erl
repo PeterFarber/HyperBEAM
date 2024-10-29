73a5db1c21a99a9a35ae1e64999df598ba958638
Peter Farber <farberpete@gmail.com>
2024-10-29 15:34:05 -0400

    feat(ao_app): added attest_key func to use gotpm attest on 0 padded wallet addr

--- src/ao_app.erl
+++ src/ao_app.erl
@@ -8,6 +8,7 @@
 -behaviour(application).
 
 -export([start/2, stop/1]).
+-export([attest_key/0]).
 
 -include("include/ao.hrl").
 
@@ -21,5 +22,38 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
+
+pad_to_64(Addr) ->
+    % Encode Addr as a hex string and convert to list for length calculation
+    HexAddr = binary_to_list(binary:encode_hex(Addr)),
+    % Calculate padding if necessary and pad with leading zeros
+    Padding = 64 - length(HexAddr),
+    lists:duplicate(Padding, $0) ++ HexAddr.
+
+attest_key() ->
+    W = ao:wallet(),
+    Addr = ar_wallet:to_address(W),
+    PaddedAddr = pad_to_64(Addr),
+    Cmd = lists:flatten(io_lib:format("sudo gotpm attest --key AK --nonce ~s", [PaddedAddr])),
+    CommandResult = os:cmd(Cmd),
+    case is_list(CommandResult) of
+        true ->
+            % If CommandResult is a list of integers, convert it to binary
+            BinaryResult = list_to_binary(CommandResult),
+            Signed = ar_bundles:sign_item(
+                #tx{
+                    tags = [
+                        {<<"Type">>, <<"TEE-Attestation">>},
+                        {<<"Address">>, ar_util:id(Addr)}
+                    ],
+                    data = BinaryResult
+                },
+                W
+            ),
+            ao_client:upload(Signed),
+            ok;
+        false ->
+            {error, "Unexpected output format from gotpm attest command"}
+    end.
 
 %% internal functions
